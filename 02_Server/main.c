23796462f46a49ebeedef1973bd111f188c47cbd
LukiRoth <96024428+GameOfZed@users.noreply.github.com>
2023-12-28 13:51:01 +0100

    Add WebSocket server and device control functions
    Initialized and managed a WebSocket server.
    Implemented signal handling for graceful shutdowns.
    Processed client commands for device actions like toggling lamps.
    Managed client connections and error handling.
    Added client-side script for sending commands.

--- 02_Server/main.c
+++ 02_Server/main.c
@@ -49,7 +49,10 @@ typedef int int32_t;
 #define FALSE 0
 
 #define SERVER_PORT 8000		// Port number for the server
-#define BACKLOG 10 				// Number of allowed connections
+#define BACKLOG 5 				// Number of allowed connections
+#define RX_BUFFER_SIZE 1024   // Buffer size for receiving data, maybe 1024
+#define WS_HS_ACCLEN 256      // Buffer size for WebSocket handshake response
+
 
 //----- Function prototypes ----------------------------------------------------
 static void shutdownHook (int32_t sig);
@@ -118,9 +121,55 @@ int main(int argc, char **argv) {
 
 	// Main Loop
 	while (eShutdown == FALSE) {
-		
-		//usleep(1000);
 		sleep(1);
+
+		// Accept a connection
+		com_sock_id = accept(server_sock_id, (struct sockaddr *)&client, &addrlen);
+		if (com_sock_id < 0) {
+			perror("Accept failed");
+			continue;
+		}
+
+		// Read the client's message
+		char rxBuf[RX_BUFFER_SIZE];
+		int rx_data_len = recv(com_sock_id, (void *)rxBuf, RX_BUFFER_SIZE, MSG_DONTWAIT);
+
+		if (rx_data_len > 0) {
+			rxBuf[rx_data_len] = '\0';
+
+			// Is the message a handshake request
+			if (strncmp(rxBuf, "GET", 3) == 0) {
+				// Yes -> create the handshake response and send it back
+				char response[WS_HS_ACCLEN];
+				get_handshake_response(rxBuf, response);
+				send(com_sock_id, (void *)response, strlen(response), 0);
+			} else {
+				// No -> decode incoming message, process the command and send back an acknowledge message
+
+				// Process command
+				char command[rx_data_len];
+				decode_incoming_request(rxBuf, command);
+				processCommand(command);
+
+				// Send acknowledgment
+				char response[] = "<Command executed>";
+				char codedResponse[strlen(response) + 2];
+				code_outgoing_response(response, codedResponse);
+				send(com_sock_id, (void *)codedResponse, strlen(codedResponse), 0);
+			}
+		} 
+		else if (rx_data_len == 0) {
+			printf("Connection closed\n");
+			fflush(stdout);
+		}
+		else {
+			perror("Receive failed");
+		}
+		close(com_sock_id); // Close the client socket after handling
+		
+
+		//usleep(1000);
+		
 	}
 
 	// Close Webhouse
@@ -155,4 +204,60 @@ static void shutdownHook(int32_t sig) {
     printf("Ctrl-C pressed....shutdown hook in main\n");
     fflush(stdout);
     eShutdown = TRUE;
+}
+
+/*******************************************************************************
+ *  function :    processCommand
+ ******************************************************************************/
+/** \brief        Process the command received from the client
+ * 
+ * \type         static
+ * 
+ * \param[in]    commandJSON    command received from the client
+ * 
+ * \return       void
+ * 
+ ******************************************************************************/
+static void processCommand(char *commandJSON) {
+    json_error_t error;
+    json_t *root = json_loads(commandJSON, 0, &error);
+    
+    if (!root) {
+        fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+        return;
+    }
+
+    // Extract action type and other parameters
+    json_t *action = json_object_get(root, "action");
+    if (!json_is_string(action)) {
+        fprintf(stderr, "error: missing or invalid action\n");
+        json_decref(root);
+        return;
+    }
+
+	// Extract action type
+    const char *actionStr = json_string_value(action);
+
+    // Implement different actions
+    if (strcmp(actionStr, "toggleLamp") == 0) {
+        json_t *lampId = json_object_get(root, "lampId");
+        json_t *status = json_object_get(root, "status");
+
+        if (json_is_integer(lampId) && json_is_string(status)) {
+            int lamp = (int) json_integer_value(lampId);
+            const char *statusStr = json_string_value(status);
+
+            if (lamp == 1) {
+                if (strcmp(statusStr, "on") == 0) {
+                    turnLED1On();
+                } else {
+                    turnLED1Off();
+                }
+            }
+            //TODO: Implement other lamps
+        }
+    }
+    //TODO: Implement other actions
+
+    json_decref(root);
 }
