706a6de9335eee259b04ce36f1de62a7baa26b7f
LukiRoth <96024428+GameOfZed@users.noreply.github.com>
2023-12-28 12:24:53 +0100

    Implement server initialization and main loop for webhouse control
    - Added `initServer` function to set up the server socket, bind it to port 8000, and put it into listening mode.
    - Modified `main` function to include server initialization using `initServer`. Now it handles incoming client connections and sets up a signal handler for graceful shutdown.
    - Integrated `Webhouse.h` functions for controlling the hardware based on client requests.
    - Ensured robust error handling and resource management for server operations.

--- 02_Server/main.c
+++ 02_Server/main.c
@@ -48,6 +48,9 @@ typedef int int32_t;
 #define TRUE 1
 #define FALSE 0
 
+#define SERVER_PORT 8000		// Port number for the server
+#define BACKLOG 10 				// Number of allowed connections
+
 //----- Function prototypes ----------------------------------------------------
 static void shutdownHook (int32_t sig);
 
@@ -78,21 +81,60 @@ int main(int argc, char **argv) {
 	
 	signal(SIGINT, shutdownHook);
 
+	// Initialize Webhouse
 	initWebhouse();
     printf("Init Webhouse\n");
 	fflush(stdout);
 
+	//--- Initialize Server
+    
+	// Create a socket
+    server_sock_id = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock_id < 0) {
+        perror("Socket creation failed");
+        return(EXIT_FAILURE);
+    }
+
+	// Set server address
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(SERVER_PORT);
+
+	// Bind the socket
+	bind_status = bind(server_sock_id, (struct sockaddr *)&server, sizeof(server));
+    if (bind_status < 0) {
+        perror("Socket bind failed");
+        close(server_sock_id);
+        return(EXIT_FAILURE);
+    }
+
+	// Listen on the socket
+	listen_status = listen(server_sock_id, BACKLOG);
+    if (listen_status < 0) {
+        perror("Listen failed");
+        close(server_sock_id);
+        return(EXIT_FAILURE);
+    }
+
+	// Main Loop
 	while (eShutdown == FALSE) {
-		printf("Main Loop\n");
-		fflush(stdout);
 		
 		//usleep(1000);
 		sleep(1);
 	}
 
+	// Close Webhouse
 	closeWebhouse();
 	printf ("Close Webhouse\n");
 	fflush (stdout);
+
+	// Close socket
+	if(close(server_sock_id) < 0) {
+		perror("Socket close failed");
+	} else {
+		printf("Socket closed\n");
+		fflush(stdout);
+	}
 
 	return EXIT_SUCCESS;
 }
